5e13e80f5511d2d7ae88b5c6e3c2c248e18bb52e
Mohsin Hassan <86055307+aboutmohsin@users.noreply.github.com>
2023-08-23 22:44:24 +0500

    fixed vercel issue

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/components/home/HeroSection";
 import NavBar from "@/components/navbar/NavBar";
 import { client } from "@/lib/SanityClient";
 import Image from "next/image";
-export const getProductData = async () => {
+const getProductData = async () => {
   const res = await client.fetch(`*[_type=="product"]{
     _id,
     title,
@@ -20,15 +20,17 @@ export default async function Home() {
   const data: IProduct[] = await getProductData();
   console.log("Sanity Product data:", data);
   return (
-    <div className="mx-16">
+    <div>
+      <div className="max-w-5xl">
+        <NavBar />
+        <HeroSection />
+      </div>
       {/* {data.map((item, index) => (
         <div key={item.id}>
           title:{item.title}
           discription:{item.description}
         </div>
       ))} */}
-      <NavBar />
-      <HeroSection />
     </div>
   );
 }
