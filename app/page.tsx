c20dbc8644fba6288401caed64cc4f09b9dea15e
Mohsin Hassan <86055307+aboutmohsin@users.noreply.github.com>
2023-08-23 22:50:20 +0500

    fixed vercel issue

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,11 @@ import HeroSection from "@/components/home/HeroSection";
 import NavBar from "@/components/navbar/NavBar";
 import { client } from "@/lib/SanityClient";
 import Image from "next/image";
-const getProductData = async () => {
-  const res = await client.fetch(`*[_type=="product"]{
-    _id,
-    title,
-    description,
 
-  }`);
-  return res;
-};
-interface IProduct {
-  id: string;
-  title: string;
-  description: string;
-}
 export default async function Home() {
-  const data: IProduct[] = await getProductData();
-  console.log("Sanity Product data:", data);
   return (
     <div>
-      <div className="max-w-5xl">
+      <div className="max-w-[1100px] mx-auto">
         <NavBar />
         <HeroSection />
       </div>
--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
   return (
     <div className="flex items-center my-8 gap-20">
       <div className="w-2/5 space-y-4">
-        <p className="bg-blue-300 text-blue-500 rounded-md px-2 py-4 font-semibold w-1/5">
+        <p className="bg-blue-300 text-blue-500 rounded-md p-4  font-semibold w-1/4">
           Sale 70%
         </p>
         <h1 className="font-bold text-6xl ">
@@ -18,7 +18,7 @@ export default function HeroSection() {
           Anyone can beat you but no one can beat your outfit as long as you
           wear Dine outfits.
         </p>
-        <button className="flex items-center justify-center bg-slate-300 py-4 rounded-md gap-4 w-1/3">
+        <button className="flex items-center justify-center bg-slate-300 py-4 rounded-md gap-4 w-2/5">
           <Image src={ShopingCart} alt="Shoping Cart" className="w-1/6" />
           <p>Shopping Cart</p>
         </button>
