098f402c0d478bda29802a314fb267464221ee2a
Mohsin Hassan <86055307+aboutmohsin@users.noreply.github.com>
2023-08-26 23:29:24 +0500

    home route

--- modules/navbar/Navbar.tsx
+++ modules/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import NavbarMobile from "@/components/navbar/NavbarMobile";
 import React, { useState } from "react";
 import skutpa from "@/assets/home/skutpa-logo.png";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,7 +17,9 @@ export default function Navbar() {
       </div>
 
       <div className="flex justify-between items-center xs:hidden md:flex ">
-        <Image src={skutpa} alt="skutpa logo" className="w-1/12" />
+        <Link href={"/"}>
+          <Image src={skutpa} alt="skutpa logo" className="w-1/12" />
+        </Link>
         <NavBar />
       </div>
     </>
