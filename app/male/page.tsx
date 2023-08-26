0b4248624712995ad2c89f24f9884d672bb25f3f
Mohsin Hassan <86055307+aboutmohsin@users.noreply.github.com>
2023-08-26 23:13:00 +0500

    Male Products Folder Structure

--- app/male/page.tsx
+++ app/male/page.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import MaleProducts from "@/modules/maleproducts/maleproducts";
+import React from "react";
 
 export default function page() {
-  return (
-    <div>page</div>
-  )
+  return <MaleProducts />;
 }
---
+++ modules/maleproducts/maleproducts.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MaleProducts() {
+  return (
+    <div>maleproduct</div>
+  )
+}
